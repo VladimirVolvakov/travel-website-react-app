2a9d9c3cce1f88fd4528cfcebf2c702048655f96
VladimirVolvakov <dr.volvakov@gmail.com>
2022-10-12 21:00:26 +0300

    feat: add layout of burger menu in Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@
 import { BiSearch } from "react-icons/bi";
 import { BsPerson } from "react-icons/bs";
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import { FaFacebook, FaInstagram, FaPinterest, FaTwitter, FaYoutube } from "react-icons/fa";
 // Styles:
 import "./Navbar.styles.css";
 
@@ -22,11 +23,34 @@ const Navbar = () => {
         <BiSearch className="icon" />
         <BsPerson className="icon" />
       </div>
-      <div className="burger-menu">
+      <div className="burger-menu-closed">
         <HiOutlineMenuAlt4 className="icon" />
+      </div>
+
+      <div className="burger-menu-opened">
+        <ul className="burger-menu-nav">
+          <li>Home</li>
+          <li>Destinations</li>
+          <li>Travel</li>
+          <li>Booking</li>
+          <li>Views</li>
+        </ul>
+        <div className="burger-menu-bottom">
+          <div className="burger-menu-icons">
+            <button>Search</button>
+            <button>Account</button>
+          </div>
+          <div className="social-media-icons">
+            <FaFacebook className="icon" />
+            <FaInstagram className="icon" />
+            <FaTwitter className="icon" />
+            <FaPinterest className="icon" />
+            <FaYoutube className="icon" />
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Navbar; 
+export default Navbar;
