422a44110925f821deaf71b04d9ba87f2229f695
VladimirVolvakov <dr.volvakov@gmail.com>
2022-10-13 17:40:33 +0300

    feat: add markup for Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 // Styles: 
 import "./Hero.styles.css";
+// Icon:
+import { AiOutlineSearch } from "react-icons/ai";
 // Video:
 import Video from "../../assets/maldivesVideo.mp4";
 
@@ -10,6 +12,18 @@ const Hero = () => {
         <source src={Video} type="video/mp4" />
       </video>
       <div className="overlay"></div>
+      <div className="content">
+        <h1>First Class Travel</h1>
+        <h2>Top Locations Worldwide</h2>
+        <form className="form">
+          <div>
+            <input type="text" placeholder="Search Your Destination" />
+          </div>
+          <div>
+            <button><AiOutlineSearch className="icon" /></button>
+          </div>
+        </form>
+      </div>
     </section>
   );
 };
