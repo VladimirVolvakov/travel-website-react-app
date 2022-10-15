c49f81944ae047d117f00b48f4558a4f76c51b7b
VladimirVolvakov <dr.volvakov@gmail.com>
2022-10-15 14:57:29 +0300

    feat: add markup of Destinations component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 // Components:
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
+import Destinations from './components/Destinations/Destinations';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Destinations />
     </div>
   );
 }
---
+++ src/components/Destinations/Destinations.jsx
@@ -0,0 +1,28 @@
+// Styles:
+import "./Destinations.styles.css";
+// Images:
+import BoraBora from "../../assets/borabora.jpg";
+import BoraBora2 from "../../assets/borabora2.jpg";
+import Maldives from "../../assets/maldives.jpg";
+import Maldives2 from "../../assets/maldives2.jpg";
+import KeyWest from "../../assets/keywest.jpg";
+
+const Destinations = () => {
+  return (
+    <section className="destinations">
+      <div className="container">
+        <h2>All-Inclusive Resorts</h2>
+        <p>On the Caribbean Best Beaches</p>
+        <div className="img-container">
+          <img src={BoraBora} alt="Bora Bora" />
+          <img src={BoraBora2} alt="Bora Bora" />
+          <img src={Maldives} alt="Maldives" />
+          <img src={Maldives2} alt="Maldives" />
+          <img src={KeyWest} alt="Key West" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Destinations;
